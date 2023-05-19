3ca048fb7fc8d110b240280bcd4c5fa593e433e8
ZiyaOzgul <ziya.d.ozgul@gmail.com>
2023-05-20 02:19:59 +0300

    about - contact page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 
 export const About = () => {
-  return <div>About</div>;
+  return (
+    <div
+      style={{
+        backgroundImage: `url('https://akademi.icerikbulutu.com/wp-content/uploads/2020/10/hakkimizda-yazisi.jpg')`,
+      }}
+      className="h-screen  w-full bg-no-repeat bg-scroll bg-cover flex items-center justify-start"
+    >
+      <div className="flex container items-start justify-start flex-col space-y-6 lg:space-y-12 w-1/3">
+        <h1 className="font-bold text-4xl text-white text-center mx-auto">
+          Biz Kimiz
+        </h1>
+        <p className="font-medium text-lg px-12">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga
+          recusandae necessitatibus dolore deleniti iure tempora deserunt
+          suscipit mollitia, esse sint dolores, soluta, fugit quis repellat
+          nesciunt pariatur expedita rem perferendis! Lorem ipsum, dolor sit
+          amet consectetur adipisicing elit. Aspernatur impedit corporis dolor
+          quis dolore error dolorem, saepe harum voluptatem! Nobis fugiat
+          temporibus minus enim sed quia excepturi earum, voluptatum inventore.
+        </p>
+      </div>
+    </div>
+  );
 };
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,5 +1,60 @@
 import React from "react";
-
+import { UilPhone, UilMapMarkerAlt } from "@iconscout/react-unicons";
 export const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="flex items-start justify-start  mt-4 lg:mt-12 flex-col ">
+      <div className="flex flex-col-reverse items-center justify-start space-y-6 lg:space-y-12 px-6 py-4 basis-1">
+        <p className="text-bold font-medium mb-4">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem,
+          sequi esse aperiam sed distinctio debitis modi expedita voluptatibus
+          illum illo odio sit deleniti quidem? Ipsam exercitationem tempora
+          aliquid optio non? Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. A quod minus dolor delectus laborum id eaque adipisci ipsa,
+          deleniti laudantium doloribus error aliquam sequi deserunt nihil in
+          fuga animi qui? Lorem ipsum dolor sit amet consectetur, adipisicing
+          elit. Quibusdam adipisci beatae culpa. Similique dolores deserunt
+          suscipit laudantium, ab aspernatur nemo veniam neque ea, nesciunt
+          tenetur, saepe unde mollitia delectus laboriosam? Lorem ipsum dolor
+          sit amet consectetur, adipisicing elit. Alias delectus aliquid commodi
+          accusantium tenetur neque exercitationem maiores sit voluptates minus,
+          officia magnam voluptate voluptatem, et iure odit pariatur, enim esse?
+        </p>
+        <h1 className="text-4xl font-bold uppercase text-red-500 font-sans py-12">
+          Bize Ulaşın
+        </h1>
+        <img
+          src="https://www.halkbank.com.tr/tr/bize-ulasin/_jcr_content/root/responsivegrid/responsivegrid/responsivegrid_780183488/teaser.coreimg.jpeg/1614260089246/yatirimci-iletisimi.jpeg"
+          alt=""
+        />
+      </div>
+      <div className="flex items-center justify-start space-x-4">
+        <div className="flex items-center justify-evenly spcae-x-6 lg:space-x-12 basis-1/2 border border-gray-100 shadow-xl rounded-lg">
+          <div className="flex flex-col items-start justify-start space-y-6 px-4 container my-6 basis-1/2">
+            <h1 className="font-bold text-4xl text-red-500">Bizi Arayın</h1>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eum
+              modi facere reiciendis incidunt animi neque distinctio
+              consequuntur voluptatem in doloremque odit numquam quidem magnam
+              vero impedit quod, quae ipsam?
+            </p>
+          </div>
+          <UilPhone className="hover:scale-110 hover:text-red-500 transition-all duration-500 ease-in-out cursor-pointer"></UilPhone>
+        </div>
+        <div className="flex items-center justify-evenly spcae-x-6 lg:space-x-12 basis-1/2 border border-gray-100 shadow-xl rounded-lg ">
+          <div className="flex flex-col items-start justify-start space-y-6 px-4 container my-6 basis-1/2 ">
+            <h1 className="font-bold text-4xl text-red-500">
+              Bizi Ziyaret Edin
+            </h1>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eum
+              modi facere reiciendis incidunt animi neque distinctio
+              consequuntur voluptatem in doloremque odit numquam quidem magnam
+              vero impedit quod, quae ipsam?
+            </p>
+          </div>
+          <UilMapMarkerAlt className="hover:scale-110 hover:text-red-500 transition-all duration-500 ease-in-out cursor-pointer"></UilMapMarkerAlt>
+        </div>
+      </div>
+    </div>
+  );
 };
